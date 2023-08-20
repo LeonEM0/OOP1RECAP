bc991043baa42236f6d96ccbefdba22b23b41e37
Admin <leonnegro0101@gmail.com>
2023-08-19 22:43:05 -0400

    Commit generics

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -11,8 +11,50 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Generics");
+            int[] arrayint = { 1, 2, 3 };
+            double[] doublearray = { 1.1, 1.2, 1.3 };
+            string[] stringarray = { "uno", "dos", "tres" };
+            char[] chararray = {'|','Z','E'};
 
+
+            Console.WriteLine("Generics\n");
+            DisplayElements(arrayint);
+            DisplayElements(doublearray);
+            DisplayElements(stringarray);
+            DisplayElements(chararray);
+
+            Console.ReadKey();
         }
+
+        public static void DisplayElements<T>(T[] array)
+        {
+            foreach(T number in array)
+            {
+                Console.WriteLine(number);
+            }
+        }
+/*
+        public static void DisplayElements(int[] array)
+        {
+            foreach(int numbers in array)
+            {
+                Console.WriteLine(numbers);
+            }
+        }
+        public static void DisplayElements(double[] array)
+        {
+            foreach (double numbers in array)
+            {
+                Console.WriteLine(numbers);
+            }
+        }
+        public static void DisplayElements(string[] array)
+        {
+            foreach (string numbers in array)
+            {
+                Console.WriteLine(numbers);
+            }
+        }
+*/
     }
 }
