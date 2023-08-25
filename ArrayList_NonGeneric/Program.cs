15a96dad50123092acfb341da77bbf0d78eb4ce5
Admin <leonnegro0101@gmail.com>
2023-08-25 18:05:52 -0400

    ArrayList ALLOWS MULTIPLE DATA TYPES

---
+++ ArrayList_NonGeneric/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; // You will need the library system.Collections to use ArrayList
+namespace ArrayList_NonGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contain = true;
+            ArrayList jugadores = new ArrayList();
+            Console.WriteLine(" ArrayList NonGeneric");
+            Console.WriteLine(" An ArrayList stores elements of 'MULTIPLE' data types whose size can be changed dinamically");
+            jugadores.Add(" Leon ");
+            jugadores.Add(100);
+            jugadores.Add('$');
+            jugadores.Add(1.75f+"cm"); //you can introduce multiple data types 
+            jugadores.Add(25+" Years");
+            jugadores.Add(3+" Lives");
+            jugadores.Add("$"+6000+" CAD");
+            jugadores.Add("$"+100 +" MXN");
+            jugadores.Add(100 + "Bullets");
+            jugadores.Add("item");
+
+            jugadores.Remove('$');
+            for(int i = 0; i<jugadores.Count;i++)
+            {
+                Console.WriteLine(jugadores[i]);
+            }
+            /*
+            foreach(var jugadordata in jugadores)
+            {
+                Console.WriteLine(jugadordata);
+            }
+            */
+            Console.WriteLine("Los datos de los jugadores es "+jugadores.Count);
+
+            //To Access with the index
+            Console.WriteLine(jugadores[5]);
+            Console.WriteLine(jugadores[7]);
+            Console.WriteLine(jugadores[6]);
+            //Tochange the values in the index
+            jugadores[0] = "Emmanuel";
+            jugadores[1] = 200;
+
+            Console.WriteLine(jugadores[0]);
+            Console.WriteLine(jugadores[1]);
+            //
+            contain = jugadores.Contains(200);
+
+            Console.WriteLine("The element exists? " + contain);
+
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+
+        }
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -13,7 +13,12 @@ namespace Collections
             List<int> numbers = new List<int>() {100,200,300,400,500};
             List<string> subjects = new List<string> {"1 - Operating systems","2 - Knowledge","3 - Databases","4 - oop2","5 - Applied Math","6 - French","7 - Physical education","8 - Game Engine" };
             var activities = new List<string> {"Sleep","Study","Play Video Games","Coding","Exercise"};
-            List<Players> player = new List<Players>(); 
+            List<Players> player = new List<Players>();
+            char[] symbology = { 'Z', '$', '%' };
+            string[] MarriedPeople = {"Yes","No","No","No","No","Yes","Yes"};
+            int married = 0;
+            int unmarried = 0;
+
             Console.WriteLine("COLLECTIONS!");
             Console.WriteLine("You need to add system collections. Generic library");
             Console.WriteLine("Similar to an array but, IT CAN DYNAMICALLY INCREASE OR DECREASE IN SIZE ");
@@ -31,18 +36,20 @@ namespace Collections
             subjects.Add("9 - English");
           //  subjects.Remove("5 - Applied Math"); // or subjects.Remove(subjects[5]);
 
-            subjects.Insert(9,"10 - Literary Genres");
+            subjects.Insert(9,"Litery genres");
             subjects.Remove("9 - English");
             subjects.RemoveAt(6);
 
             foreach (int numeros in numbers)//foreach
             {
+                Console.WriteLine("Collection of Numbers");
                 Console.WriteLine(numeros);
             }
             Console.WriteLine("\n");
 
             for(int i = 0; i<subjects.Count;i++)//for
             {
+                Console.WriteLine("Collection of objects");
                 Console.WriteLine(subjects[i]);
 
             }
@@ -57,13 +64,39 @@ namespace Collections
                 Console.WriteLine(p.username);
             }
             Console.WriteLine("\n");
+            
+            foreach(char symbol in symbology)
+            {
+                Console.WriteLine(symbol);
+            }
+            Console.WriteLine();
+            foreach(string civil_status in MarriedPeople)
+            {
+                 
+                Console.WriteLine(civil_status);
+                if(civil_status == "Yes")
+                {
+                    married++;
+                    
+                }
+                else if(civil_status == "No")
+                {
+                    unmarried ++;
+                }
+
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("The amount of Married people is: "+married);
+            Console.WriteLine("The amount of Unmarried people is: "+unmarried);
+
 
 
             Console.WriteLine("AMOUNT OF NUMBERS "+numbers.Count); //Count indicates tbe amount of elements in the List
             Console.WriteLine("AMOUNT OF SUBJECTS "+subjects.Count); //Count 
 
-             
 
+            Console.ReadKey();
         }
     }
 }
