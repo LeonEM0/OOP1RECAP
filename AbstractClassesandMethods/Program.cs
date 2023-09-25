527af41afb19c052fed35ebb50695447ad2a0cf5
LeonEM0 <117362597+LeonEM0@users.noreply.github.com>
2023-09-24 21:31:36 -0400

    adding playerscriptfunctional

---
+++ AbstractClassesandMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesandMethods
+{
+    interface IAcidAttack
+    {
+         void LaunchAcid();
+        
+    }
+
+
+    class Enemy
+    {
+        public Enemy()
+        {
+            Console.WriteLine("Constructor ENEMIGO");
+        }
+    }
+    class SoldierEnemy:Enemy,IAcidAttack;
+    {
+        public SoldierEnemy()
+        {
+            Console.WriteLine("Constructor SOLDADO! ENEMIGO!");
+        }
+        public void LaunchAcid()
+        {
+            Console.WriteLine("The enemy is launching acid");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoldierEnemy soldadomalo1 = new SoldierEnemy();
+
+            Console.ReadKey();
+        }
+    }
+}
